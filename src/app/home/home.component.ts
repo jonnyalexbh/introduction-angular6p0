fd32e9fe360f885f18c2728d684449f1f99eb35d
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-15 13:52:09 -0500

    exercise with services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,21 +14,40 @@ import { ClothesService } from '../services/clothes.services';
 *
 */
 export class HomeComponent {
+
   public title = 'Homepage';
+  public clothing_list: Array<string>;
+  public hold_save: string;
   /**
-   * constructor
-   *
-   */
+  * constructor
+  *
+  */
   constructor(
     private _clothesService: ClothesService
   ) {
+
   }
   /**
   * ngOnInit - first method that is executed after the constructor
   *
   */
   ngOnInit() {
-    this._clothesService.test('Jean');
-    console.log(this._clothesService.test('Jean Colombia'));
+    this.clothing_list = this._clothesService.getClothes();
+    console.log(this.clothing_list);
+  }
+  /**
+  * savePledge
+  *
+  */
+  savePledge() {
+    this._clothesService.addClothes(this.hold_save);
+    this.hold_save = null;
+  }
+  /**
+  * removeGarment
+  *
+  */
+  removeGarment(index: number) {
+    this._clothesService.removeGarment(index);
   }
 }
--- src/app/services/clothes.services.ts
+++ src/app/services/clothes.services.ts
@@ -7,12 +7,36 @@ import { Injectable } from '@angular/core';
 export class ClothesService {
 
   public namePledge = 'Camiseta Nick';
+  public clothingCollection = ['White pants', 'Red shirt'];
   /**
   * test
   *
   */
   test(name) {
     return name;
+  }
+  /**
+  * addClothes
+  *
+  */
+  addClothes(name_pledge: string): Array<string> {
+    this.clothingCollection.push(name_pledge);
+    return this.getClothes();
+  }
+  /**
+  * getClothes
+  *
+  */
+  getClothes(): Array<string> {
+    return this.clothingCollection;
+  }
+  /**
+  * removeGarment
+  *
+  */
+  removeGarment(index: number) {
+    this.clothingCollection.splice(index, 1);
+    return this.getClothes();
   }
 
 }
